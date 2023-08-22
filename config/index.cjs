25d1066935c0f663250bf0ce4a34a35e05244fff
xiaoyu20020 <142811489+xiaoyu20020@users.noreply.github.com>
2023-08-22 22:16:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '5a5d531fee3d400481f9d0ec4bad3e65',
 
   CALLBACK_USERS: [
     {
